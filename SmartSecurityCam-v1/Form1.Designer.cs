e95289a69b82be50c314de6acf41b377493a8bd3
RronM <RronM@DESKTOP-UE679DE>
2025-12-08 21:08:47 +0100

    Add project files.

---
+++ SmartSecurityCam-v1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SmartSecurityCam_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listLog = new System.Windows.Forms.ListBox();
+            this.pictureBoxFeed = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listLog
+            // 
+            this.listLog.FormattingEnabled = true;
+            this.listLog.Location = new System.Drawing.Point(12, 485);
+            this.listLog.Name = "listLog";
+            this.listLog.Size = new System.Drawing.Size(776, 186);
+            this.listLog.TabIndex = 0;
+            // 
+            // pictureBoxFeed
+            // 
+            this.pictureBoxFeed.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxFeed.Name = "pictureBoxFeed";
+            this.pictureBoxFeed.Size = new System.Drawing.Size(776, 391);
+            this.pictureBoxFeed.TabIndex = 1;
+            this.pictureBoxFeed.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 409);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(190, 70);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Camera";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(217, 409);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(190, 70);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop Camera";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 681);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBoxFeed);
+            this.Controls.Add(this.listLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFeed)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listLog;
+        private System.Windows.Forms.PictureBox pictureBoxFeed;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SmartSecurityCam-v1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+
+
+namespace SmartSecurityCam_v1
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture capture;
+        private Mat frame;
+        private Mat prevGray;
+        private bool isRunning = false;
+        private Thread cameraThread;
+
+        private string snapshotFolder = "Snapshots";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(snapshotFolder))
+                    System.IO.Directory.CreateDirectory(snapshotFolder);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            capture = new VideoCapture(0);
+
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("Cannot open camera, Close other apps using the camera.");
+                return;
+            }
+
+            frame = new Mat();
+            prevGray = null;
+            isRunning = true;
+
+            cameraThread = new Thread(CameraLoop);
+            cameraThread.IsBackground = true;
+            cameraThread.Start();
+        }
+
+        private void CameraLoop()
+        {
+            while (isRunning)
+            {
+                Mat temp = new Mat();
+
+                if (!capture.Grab())
+                {
+                    Thread.Sleep(5);
+                    continue;
+                }
+
+                // Safe Retrieve (pulls the grabbed frame)
+                capture.Retrieve(temp);
+
+                if (temp.Empty())
+                    continue;
+
+                // Use temp as the current frame
+                frame?.Dispose();
+                frame = temp.Clone();
+
+                // Motion detection
+
+                using (Mat gray = new Mat())
+                {
+                    Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+                    Cv2.GaussianBlur(gray, gray, new OpenCvSharp.Size(21, 21), 0);
+
+                    if (prevGray != null)
+                    {
+                        using (Mat diff = new Mat())
+                        {
+                            Cv2.Absdiff(prevGray, gray, diff);
+                            Cv2.Threshold(diff, diff, 25, 255, ThresholdTypes.Binary);
+
+                            int count = Cv2.CountNonZero(diff);
+
+                            if (count > 5000)
+                            {
+                                listLog.Invoke(new Action(() =>
+                                {
+                                    listLog.Items.Add($"Motion detected at {DateTime.Now}");
+                                    listLog.TopIndex = listLog.Items.Count - 1;
+                                }));
+                            }
+                        }
+                    }
+
+                    prevGray?.Dispose();
+                    prevGray = gray.Clone();
+                }
+
+                // Show camera feed
+
+                byte[] bmpData = frame.ImEncode(".bmp");
+                using (var ms = new System.IO.MemoryStream(bmpData))
+                {
+                    Bitmap bitmap = new Bitmap(ms);
+                    pictureBoxFeed.Invoke(new Action(() =>
+                    {
+                        pictureBoxFeed.Image?.Dispose();
+                        pictureBoxFeed.Image = bitmap;
+                    }));
+                }
+            }
+
+            // Cleanup after the loop completes
+            capture?.Release();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+
+            if (cameraThread != null && cameraThread.IsAlive) 
+            {
+                int waited = 0;
+                while (cameraThread.IsAlive && waited < 2000)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(50);
+                    waited += 50;
+                }
+            }
+            capture?.Release();
+            capture?.Dispose();
+            capture = null;
+
+            frame?.Release();
+            frame = null;
+
+            prevGray?.Dispose();
+            prevGray = null;
+
+            if (pictureBoxFeed.InvokeRequired)
+            {
+                pictureBoxFeed.Invoke(new Action(() =>
+                {
+                    pictureBoxFeed.Image?.Dispose();
+                    pictureBoxFeed.Image = null;
+                }));
+            }
+            else
+            {
+                pictureBoxFeed?.Dispose();
+                pictureBoxFeed = null;
+            }
+
+            if (listLog.InvokeRequired)
+            {
+                listLog.Invoke(new Action(() =>
+                {
+                    listLog.Items.Add("Camera Stopped");
+                }));
+            }
+            else
+            {
+                listLog.Items.Add("Camera Stopped");
+            }
+        }
+    }
+}
